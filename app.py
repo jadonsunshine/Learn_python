130eede0621f81ae7b76cb33af63b11f9b23adda
Jadon Sunshine <109299010+jadonsunshine@users.noreply.github.com>
2023-08-15 13:42:32 +0100

    an example of a python introduction i've had for a class

---
+++ app.py
@@ -0,0 +1,13 @@
+print("Hello World") 
+name = "Jadon"
+print(name);
+birthyear = int(input("Enter your birth year :   "))
+age = 2023 - birthyear
+print(age)
+
+# now lets build a simple calculator
+first_Number = float(input("Please input first number: "));
+second_Number = float(input("Please input second number: "));
+Add = first_Number + second_Number;
+result = "Sum : "+ str(Add)
+print(result)200
